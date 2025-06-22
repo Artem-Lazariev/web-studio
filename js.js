4c3ead21ce9ebc719baa4786da43abb6785d7bb3
Lazarev Artem <lazarievartem25@gmail.com>
2025-06-22 11:49:47 +0300

    upgrade header

---
+++ js.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const modal = document.getElementById("modal");
+    const contactLink = document.getElementById("contactLink");
+    const closeBtn = document.getElementById("modalClose");
+
+    contactLink.addEventListener("click", (e) => {
+        e.preventDefault();
+        modal.style.display = "block";
+    });
+
+    closeBtn.addEventListener("click", () => {
+        modal.style.display = "none";
+    });
+
+    window.addEventListener("click", (e) => {
+        if (e.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+});
+
